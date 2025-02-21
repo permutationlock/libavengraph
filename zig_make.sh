24a219e75eb022cac242be025e9df175d83e3ba8
Aven Bross <kieroda@gmail.com>
2025-02-21 15:55:53 -0600

    Removed unnecessary option from zig_make.sh

--- zig_make.sh
+++ zig_make.sh
@@ -1,7 +1,6 @@
 #! /bin/bash
 zig cc -DAVEN_BUILD_COMMON_DEFAULT_CC="\"zig\"" \
     -DAVEN_BUILD_COMMON_DEFAULT_AR="\"zig\"" \
-    -DAVEN_BUILD_COMMON_DEFAULT_WINDRES="\"zig\"" \
     -DAVEN_BUILD_COMMON_DEFAULT_CCFLAGS="\"cc -std=c11\"" \
     -DAVEN_BUILD_COMMON_DEFAULT_LDFLAGS="\"cc\"" \
     -DAVEN_BUILD_COMMON_DEFAULT_ARFLAGS="\"ar -rcs\"" \
